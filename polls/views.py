b7cbf781b710f3d6a7990aac0481a1c9662030b7
VanoAkofashvili <vanikoakofa@gmail.com>
2021-03-03 21:48:39 +0400

    with generic views

--- polls/views.py
+++ polls/views.py
@@ -7,31 +7,48 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')
-    template = loader.get_template('polls/index.html')
-    context = {
-        'question_list': latest_question_list
-    }
-    return HttpResponse(template.render(context, request))
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'question_list': latest_question_list
+#     }
+#     return HttpResponse(template.render(context, request))
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("question does not exist")
-
-    return render(request, 'polls/detail.html', {
-        'question': question
-    })
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-    question = Question.objects.get(pk=question_id)
-    return render(request, 'polls/results.html', context={
-        'question': question
-    })
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("question does not exist")
+#
+#     return render(request, 'polls/detail.html', {
+#         'question': question
+#     })
+#
+#
+# def results(request, question_id):
+#     question = Question.objects.get(pk=question_id)
+#     return render(request, 'polls/results.html', context={
+#         'question': question
+#     })
 
 
 def vote(request, question_id):
